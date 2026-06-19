e3c17e1417113fdf17deb66179228f4c9a11ce47
tony <tonyantony300@gmail.com>
2026-06-19 18:54:16 +0530

    assets: update favicons

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -113,18 +113,12 @@ export async function generateMetadata({ params }) {
     classification: "Desktop Application",
     icons: {
       icon: [
-        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-        { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
-        { url: "/favicon-192x192.png", sizes: "192x192", type: "image/png" },
-        { url: "/favicon-512x512.png", sizes: "512x512", type: "image/png" },
+        { url: "/favicon.svg", type: "image/svg+xml" },
+        { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
         { url: "/favicon.ico", sizes: "any" },
       ],
       apple: [
         { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-      ],
-      other: [
-        { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#000000" },
       ],
     },
     manifest: "/manifest.json",
